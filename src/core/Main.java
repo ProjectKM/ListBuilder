1a4dfa8c5d0c3d3f7c3cadce16cf3fb336ad4116
Robert Stevens <robert@stevens.tk>
2015-04-25 01:19:48 +0200

    init

---
+++ src/core/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author ing. R.J.H.M. Stevens
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/dataContainer/Person.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package dataContainer;
+
+import java.util.ArrayList;
+
+/**
+ * @author ing. R.J.H.M. Stevens
+ *
+ */
+public class Person {
+	private String fistName;
+	private String lastName;
+	private String scholarUrl;
+	
+	private ArrayList<Person> connections;
+	/**
+	 * 
+	 */
+	public Person() {
+	}
+
+}
